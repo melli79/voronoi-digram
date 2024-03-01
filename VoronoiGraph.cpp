51011c79066f70f44c0ce440784a5228b0ea6cea
Melchior <melchiorG@gMail.com>
2024-03-01 11:15:53 +0100

    copyright.

--- VoronoiGraph.cpp
+++ VoronoiGraph.cpp
@@ -1,6 +1,9 @@
 //
 // Created by Melchior Grützmann on 2024-02-26.
 //
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.md or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #include "VoronoiGraph.h"
 
